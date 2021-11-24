ffb2fd0faf4a69db92ae019ed6cb2540fc993528
Michal Zbiegień <77878599+ndiyathanda@users.noreply.github.com>
2021-11-24 12:53:39 +0100

    VER0.2
    NEW menu and pausing 
    several bug fixes

---
+++ game_src/player.py
@@ -0,0 +1,18 @@
+import pygame
+from CONST import *
+
+class Player(pygame.Rect):
+    def __init__(self):
+        self.x = 0
+        self.y = 150
+        self.h = 32
+        self.w = 32
+        self.hp = 3
+
+    def check_player(self):
+        if self.y<=0:
+            self.y = 10
+            print("s")
+        if self.y>=180:
+            self.y = 160
+            print("x")
